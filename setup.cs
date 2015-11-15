748dd822b72dace56c481ebefa5a156ad01eecbf
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-11-14 18:39:58 -0800

    yada-yada

---
+++ setup.cs
@@ -0,0 +1,17 @@
+using System;
+using WixSharp;
+  
+class Script 
+{
+    static public void Main()
+    {
+        var project = new Project("MyProduct",
+                          new Dir(@"%ProgramFiles%\My Company\My Product",
+                              new File(@"Files\Docs\readme.txt"),
+                              new File(@"Files\Bin\MyApp.exe")));
+ 
+        project.GUID = new Guid("6f330b47-2577-43ad-9095-1861ba25889b");
+ 
+        Compiler.BuildMsi(project);
+    }
+}
